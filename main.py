6dc4e54775deab993fde5d1dc1e0b3237d2cfc98
avulaanirudhreddy <aanirudhreddy_cse220503@mgit.ac.in>
2025-08-23 19:57:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import streamlit as st
 
 load_dotenv()
 
-st.title('Founder's Commnity Dev Trial')
+st.title('Founder's Community Dev Trial')
 
 if "OPENAI_API_KEY" in st.secrets['secrets']:
     api_key = st.secrets['secrets']["OPENAI_API_KEY"]
